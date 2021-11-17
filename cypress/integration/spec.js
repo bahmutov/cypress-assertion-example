4d2b246d7e7e9625aacb1c93e6c39aa8f3612a88
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-17 07:57:57 -0500

    first tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,25 +1,19 @@
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
-describe('Example Cypress TodoMVC test', () => {
+describe('list', () => {
   beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
-    cy.visit('http://todomvc.com/examples/vue/')
+    cy.visit('cypress/integration/index.html')
+  })
+  it('has ID', () => {
+    cy.get('ul')
+      .should('have.id', 'data-attributes')
+      .and('have.css', 'list-style', 'outside none square')
   })
 
-  it('adds 2 todos', function () {
-    cy.get('.new-todo')
-      .type('learn testing{enter}')
-      .type('be cool{enter}')
-    cy.get('.todo-list li').should('have.length', 2)
+  context('item', () => {
+    it('has test id data attribute', () => {
+      cy.contains('li', 'first').should('have.attr', 'data-test-id', 'first')
+      cy.contains('li', 'first').and('have.css', 'font-weight', '700')
+    })
   })
-
-  // more examples
-  //
-  // https://github.com/cypress-io/cypress-example-todomvc
-  // https://github.com/cypress-io/cypress-example-kitchensink
-  // https://on.cypress.io/writing-your-first-test
 })
